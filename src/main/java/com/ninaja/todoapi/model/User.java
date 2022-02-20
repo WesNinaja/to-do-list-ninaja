2a3e4cb2c5e61bd4424df583c2f5b57758b788d7
wesleycalle <wesleyninaja@gmail.com>
2022-02-20 18:15:36 -0300

    @WesNinaja [Task: Update attributes model User]

--- src/main/java/com/ninaja/todoapi/model/User.java
+++ src/main/java/com/ninaja/todoapi/model/User.java
@@ -1,35 +1,83 @@
 package com.ninaja.todoapi.model;
 
-import javax.persistence.Column;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@Table (name = "users")
-@Getter
-@Setter
-@ToString
+@Table(name = "users")
 public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-
-	@Column(nullable = false)
+	private @Id String email;
 	private String name;
-
-	@Column(nullable = false)
-	private String email;
-
-	@Column(nullable = false)
 	private String password;
+	private String token;
+
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("user")
+	private List<Task> tasks;
+
 
 	
+	//Getters and Setters
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getToken() {
+		return token;
+	}
+
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+
+	public List<Task> getTasks() {
+		return tasks;
+	}
+
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+	
+
+	
+	
+
 }
