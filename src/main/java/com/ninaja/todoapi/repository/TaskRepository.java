0288cd937f9fa49ed8a9125d1c7131eedfe5b43d
wesleycalle <wesleyninaja@gmail.com>
2022-02-20 18:20:13 -0300

    @WesNinaja [Task: Create TaskRepository]

--- src/main/java/com/ninaja/todoapi/repository/TaskRepository.java
+++ src/main/java/com/ninaja/todoapi/repository/TaskRepository.java
@@ -1,5 +1,11 @@
 package com.ninaja.todoapi.repository;
 
-public interface TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ninaja.todoapi.model.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>{
 
 }
