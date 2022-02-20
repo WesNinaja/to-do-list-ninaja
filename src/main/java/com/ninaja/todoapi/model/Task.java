ceb29dd95183ca124823f2b0df1cd0b852d11291
wesleycalle <wesleyninaja@gmail.com>
2022-02-20 18:14:57 -0300

    @WesNinaja [Task: Update attributes model Task]

--- src/main/java/com/ninaja/todoapi/model/Task.java
+++ src/main/java/com/ninaja/todoapi/model/Task.java
@@ -1,25 +1,25 @@
 package com.ninaja.todoapi.model;
 
-import java.sql.Date;
+
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tasks")
-@Getter
-@Setter
-@ToString
 public class Task {
 
 	/*
@@ -32,13 +32,13 @@ public class Task {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false)
+	@NotNull
 	private String title;
 
-	@Column(nullable = false)
+	@NotNull
 	private String description;
 
-	@Column(nullable = false)
+	@NotNull
 	private Date deadLine;
 
 	@CreationTimestamp
@@ -48,4 +48,89 @@ public class Task {
 	@UpdateTimestamp
 	@Column(name = "updated_at")
 	private Date updatedAt;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_user")
+	@JsonIgnoreProperties("tasks")
+	private User user;
+	
+	
+	//Constructors
+	public Task() {
+	}
+	
+	public Task(Long id, String title, String description, Date deadLine, Date createdAt,
+			Date updatedAt, User user) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.deadLine = deadLine;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.user = user;
+	}
+
+
+
+
+	//Getters and Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDeadLine() {
+		return deadLine;
+	}
+
+	public void setDeadLine(Date deadLine) {
+		this.deadLine = deadLine;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
 }
