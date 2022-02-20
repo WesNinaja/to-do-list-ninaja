df81c9d889601335f9a4206d8b970830536d3217
wesleycalle <wesleyninaja@gmail.com>
2022-02-20 18:21:42 -0300

    @WesNinaja [Task: Create UserRepository]

--- src/main/java/com/ninaja/todoapi/repository/UserRepository.java
+++ src/main/java/com/ninaja/todoapi/repository/UserRepository.java
@@ -1,5 +1,15 @@
 package com.ninaja.todoapi.repository;
 
-public interface UserRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ninaja.todoapi.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	public Optional<User> findByEmail(String email);
+	
 }
