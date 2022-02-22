30902ca79e5ab068f234774345ddc5c1c9356aa8
wesleycalle <wesleyninaja@gmail.com>
2022-02-22 20:57:06 -0300

    Task: Update TaskRepository with methods findByUser, findByPrioridade

--- src/main/java/com/ninaja/todoapi/repository/TaskRepository.java
+++ src/main/java/com/ninaja/todoapi/repository/TaskRepository.java
@@ -1,11 +1,19 @@
 package com.ninaja.todoapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ninaja.todoapi.model.Task;
+import com.ninaja.todoapi.model.TaskPriority;
+import com.ninaja.todoapi.model.User;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>{
+
+	List<Task> findByUsuario(User usuario);
+
+	List<Task> findByPrioridade (TaskPriority prioridade);
 
 }
