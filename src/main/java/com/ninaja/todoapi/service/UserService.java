dd72381dd72830cb56f71b35e6d079f0c4f8a207
wesleycalle <wesleyninaja@gmail.com>
2022-02-22 22:43:20 -0300

    Create User Service

--- src/main/java/com/ninaja/todoapi/service/UserService.java
+++ src/main/java/com/ninaja/todoapi/service/UserService.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.ninaja.todoapi.helpers.PasswordValidate;
 import com.ninaja.todoapi.model.User;
 import com.ninaja.todoapi.model.dto.UserRegisterDTO;
 import com.ninaja.todoapi.repository.UserRepository;
@@ -26,7 +27,6 @@ public class UserService {
 	private UserRepository userRepository;
 	private User user;
 
-	
 	public ResponseEntity<User> registerUser(@Valid UserRegisterDTO newUser) {
 		Optional<User> optional = userRepository.findByEmail(newUser.getEmail());
 		if (optional.isPresent()) {
@@ -36,53 +36,50 @@ public class UserService {
 			user = new User();
 			user.setNome(newUser.getNome());
 			user.setEmail(newUser.getEmail());
-			user.setSenha(encryptPassword(newUser.getSenha()));
-			return ResponseEntity.status(201).body(userRepository.save(user));
+			if (PasswordValidate.validator(newUser.getSenha())) {
+				user.setSenha(encryptPassword(newUser.getSenha()));
+				return ResponseEntity.status(201).body(userRepository.save(user));
+			} else
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+						"Senha não é forte, precisa ter ter pelo 6 digitos, com números, letras minúsculas, maiúsculas e caracteres especiais.");
 		}
 	}
 
 	public List<User> listAllUsers() {
 		return userRepository.findAll();
 	}
-	
-	
 
 	public ResponseEntity<User> findUserById(Long id) {
 		return userRepository.findById(id).map(user -> ResponseEntity.ok().body(user))
 				.orElse(ResponseEntity.notFound().build());
 	}
-	
-	
 
 	public Optional<User> findByEmail(String email) {
 		return userRepository.findByEmail(email);
 	}
-	
-	
 
-	public ResponseEntity<User> updateUser(@Valid User newUser) {
-		Optional<User> optional = userRepository.findByEmail(newUser.getEmail());
+	public Optional<User> updateUser(User usuario) {
+		if (userRepository.findByEmail(usuario.getEmail()).isPresent()) {
 
-		if (optional.isPresent()) {
+			Optional<User> findUser = userRepository.findByEmail(usuario.getEmail());
+			if (PasswordValidate.validator(usuario.getSenha())) {
+				findUser.get().setSenha(encryptPassword(usuario.getSenha()));
+				findUser.get().setEmail(usuario.getEmail());
+				findUser.get().setNome(usuario.getNome());
 
-			optional.get().setNome(newUser.getNome());
-			optional.get().setSenha(encryptPassword(newUser.getSenha()));
-
-			return ResponseEntity.status(201).body(userRepository.save(optional.get()));
-		} else {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Erro ao atualizar!");
-
+				return Optional.ofNullable(userRepository.save(findUser.get()));
+			} else
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+						"Senha não é forte, precisa ter ter pelo 6 digitos, com números, letras minúsculas, maiúsculas e caracteres especiais.");
 		}
+
+		return Optional.empty();
+
 	}
-	
-	
 
 	private static String encryptPassword(String password) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		return encoder.encode(password);
 	}
-	
-	
-	
 
 }
