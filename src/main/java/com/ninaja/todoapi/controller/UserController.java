7ddfbe1656032ba3a1eb01c5e5fab4b95e0bdf16
wesleycalle <wesleyninaja@gmail.com>
2022-02-23 00:24:55 -0300

    Task: Update userRegister in UserController

--- src/main/java/com/ninaja/todoapi/controller/UserController.java
+++ src/main/java/com/ninaja/todoapi/controller/UserController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,45 +28,39 @@ import com.ninaja.todoapi.service.UserService;
 @CrossOrigin("*")
 @RequestMapping("/api/v1/users")
 public class UserController {
-	
+
 	private @Autowired UserService userService;
 	private @Autowired UserRepository repository;
-	
-	@PostMapping("/save")
-	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<User> registerUser(@Valid @RequestBody UserRegisterDTO newUser){
-		return userService.registerUser(newUser);
-	}
-	
+
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
-	public List<User>getAllUsers(){
+	public List<User> getAllUsers() {
 		return userService.listAllUsers();
 	}
-	
+
 	@GetMapping("/{id}")
 
 	public ResponseEntity<User> findById(@PathVariable Long id) {
-		return repository.findById(id)
-				.map(resp -> ResponseEntity.ok(resp))
-				.orElse(ResponseEntity.notFound().build());
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 
 	}
-	
+
 	@GetMapping("/email/{email}")
 	public ResponseEntity<Optional<User>> findByEmail(@PathVariable String email) {
 		return ResponseEntity.ok(repository.findByEmail(email));
 	}
 
+	@PostMapping("/save")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<User> registerUser(@Valid @RequestBody UserRegisterDTO newUser, Errors errors) {
+		return userService.registerUser(newUser, errors);
+	}
 
 	@PutMapping("/update")
-	public ResponseEntity<User> updateUser(@Valid @RequestBody User user){
-		
-		return userService.updateUser(user)
-			.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
-			.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-	}
-	
+	public ResponseEntity<User> updateUser(@Valid @RequestBody User user) {
 
+		return userService.updateUser(user).map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
 
 }
